d01ed1469e1783186ca589231256f437bfff526b
thomasonaistarts <ahmetzotkaci@gmail.com>
2026-05-27 23:37:11 +0100

    Update server.ts

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -2,7 +2,7 @@ import { createServerClient, type CookieOptions } from '@supabase/ssr'
 import { cookies } from 'next/headers'
 import type { Database } from '@/types/database'
 
-export async function createClient() {
+export async function await createClient() {
   const cookieStore = await cookies()
   return createServerClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
